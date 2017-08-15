bd1bd6aabf5ad01b4bc0b11603556dd64c2f6566
fajarkarim <fajarabdulkarim@gmail.com>
2017-08-15 14:53:35 +0700

    auth tested

--- server/helpers/util.js
+++ server/helpers/util.js
@@ -3,7 +3,7 @@ var jwt = require('jsonwebtoken')
 var User = require('../models/user')
 var Article = require('../models/article')
 
-var auth = (req, res, next) => {
+var authArticle = (req, res, next) => {
   let token = req.headers.token
   if (!token) {
     res.status(403).send(`please logged in first`)
@@ -15,12 +15,14 @@ var auth = (req, res, next) => {
     Article.findById(req.params.id)
     .populate('author')
     .then(article => {
-      if (article.author._id == req.params.id) {
+      console.log(`-------- ${decode.username}`);
+      if (article.author.username == decode.username) {
         next()
+      } else {
+        res.send(`its not yours`)
       }
-      next()
     })
   }
 }
 
-module.exports = { auth }
+module.exports = { authArticle }
